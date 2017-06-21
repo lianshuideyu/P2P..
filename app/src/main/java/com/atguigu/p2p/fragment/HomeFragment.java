4fe121d32a61ebc83bb571ae7302dc403ba173dc
lianshuideyu <834893479@qq.com>
2017-06-21 19:01:02 +0800

    添加async-http库，利用该库做联网请求，手动解析数据

--- app/src/main/java/com/atguigu/p2p/fragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/p2p/fragment/HomeFragment.java
@@ -4,18 +4,28 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.p2p.R;
+import com.atguigu.p2p.bean.IndexBean;
 import com.atguigu.p2p.common.AppNetConfig;
 import com.atguigu.p2p.utils.UIUtils;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 import com.youth.banner.Banner;
 import com.youth.banner.loader.ImageLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +48,8 @@ public class HomeFragment extends Fragment {
     TextView baseTitle;
     @InjectView(R.id.banner)
     Banner banner;
+
+    private IndexBean indexBean;
 
     //ImageView baseBack;
 
@@ -70,20 +82,156 @@ public class HomeFragment extends Fragment {
      * 添加数据
      */
     private void initData() {
-        //测试异常
-        //int i = 1 / 0;
+        //联网请求数据
+        getDataFromNet();
 
-        //添加banner数据
-        //设置图片加载器
+
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDataFromNet() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(AppNetConfig.INDEX, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, String content) {
+                super.onSuccess(statusCode, content);
+                Log.e("TAG", "HomeFragment联网成功==" + content);
+
+                //解析数据
+                //processData(content);
+
+
+                //手动解析
+                processData2(content);
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                super.onFailure(error, content);
+
+                Log.e("TAG", "HomeFragment联网失败==" + content);
+            }
+        });
+    }
+
+    private void processData(String json) {
+        if (!TextUtils.isEmpty(json)) {
+            indexBean = JSON.parseObject(json, IndexBean.class);
+
+            //Log.e("TAG","HomeFragment解析数据成功==" + indexBean.getProInfo().getName());
+
+            //添加banner数据
+
+
+            //添加banner数据
+            //设置图片加载器
+            initBanner();
+        } else {
+
+            Log.e("TAG", "HomeFragment解析没有数据");
+        }
+
+    }
+
+    /**
+     * 手动解析
+     * @param json
+     */
+    private void processData2(String json) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(json);
+            //最外层对象
+            indexBean = new IndexBean();
+            //imageArr数组
+            JSONArray imageArr = jsonObject.optJSONArray("imageArr");
+            //新建集合准备放数据
+            List<IndexBean.ImageArrBean> imageArrList = new ArrayList<>();
+
+            for(int i = 0; i < imageArr.length(); i++) {
+                IndexBean.ImageArrBean imageArrBean = new IndexBean.ImageArrBean();
+
+                JSONObject imageArrJSONObject = imageArr.getJSONObject(i);
+
+                String id = imageArrJSONObject.getString("ID");
+                imageArrBean.setID(id);
+                //Log.e("TAG","id==" + id);
+
+                String imapaurl = imageArrJSONObject.getString("IMAPAURL");
+                imageArrBean.setIMAPAURL(imapaurl);
+
+                String imaurl = imageArrJSONObject.getString("IMAURL");
+                imageArrBean.setIMAURL(imaurl);
+
+                //将设置好数据的对象添加到集合中
+                imageArrList.add(imageArrBean);
+            }
+
+            //将集合设置到最外层对象中
+            indexBean.setImageArr(imageArrList);
+
+            //解析proInfo对象
+            IndexBean.ProInfoBean proInfoBean = new IndexBean.ProInfoBean();
+            JSONObject jsonObject1 = jsonObject.optJSONObject("proInfo");
+
+            String id = jsonObject1.getString("id");
+            proInfoBean.setId(id);
+
+            String memberNum = jsonObject1.getString("memberNum");
+            proInfoBean.setMemberNum(memberNum);
+
+            String minTouMoney = jsonObject1.getString("minTouMoney");
+            proInfoBean.setMinTouMoney(minTouMoney);
+
+            String money = jsonObject1.getString("money");
+            proInfoBean.setMoney(money);
+
+            String name = jsonObject1.getString("name");
+            proInfoBean.setName(name);
+
+            String progress = jsonObject1.getString("progress");
+            proInfoBean.setProgress(progress);
+
+            String suodingDays = jsonObject1.getString("suodingDays");
+            proInfoBean.setSuodingDays(suodingDays);
+
+            String yearRate = jsonObject1.getString("yearRate");
+            proInfoBean.setYearRate(yearRate);
+
+
+            indexBean.setProInfo(proInfoBean);
+
+            //添加banner数据
+            initBanner();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private List<String> images;
+
+    private void initBanner() {
+        List<IndexBean.ImageArrBean> imageArr = indexBean.getImageArr();
+        //Log.e("TAG","imaurl==" + imageArr.size());
         banner.setImageLoader(new GlideImageLoader());
         //设置图片集合
-        List<String> images = new ArrayList<>();
-        images.add(AppNetConfig.BASE_URL + "images/index02.png");
+        images = new ArrayList<>();
+
+        for (int i = 0; i < imageArr.size(); i++) {
+            images.add(AppNetConfig.BASE_URL + imageArr.get(i).getIMAURL());
+            //Log.e("TAG","imaurl==" + AppNetConfig.BASE_URL + imageArr.get(i).getIMAURL());
+
+        }
+
 
         banner.setImages(images);
         //banner设置方法全部调用完毕时最后调用
         banner.start();
-
     }
 
     @Override
