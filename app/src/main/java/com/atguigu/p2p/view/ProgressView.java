cc3d67ca46004d0b8380d2697c59f80cb54ba815
lianshuideyu <834893479@qq.com>
2017-06-23 12:32:26 +0800

    自定义控件进度条圆环的画弧和文字

--- app/src/main/java/com/atguigu/p2p/view/ProgressView.java
+++ app/src/main/java/com/atguigu/p2p/view/ProgressView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,14 +18,19 @@ import com.atguigu.p2p.utils.UIUtils;
 public class ProgressView extends View {
 
     //环边 的宽度
-    int strokeWidth = UIUtils.dp2px(20);
+    int strokeWidth ;
 
     int paintColor = Color.BLUE;
 
     int width ;
     int heigth;
 
+    int progress = 60;
+    int max = 100;
+
     private Paint paint;
+
+    private float sweepAngle = 60;//圆弧进度的覆盖的角度
 
     public ProgressView(Context context) {
         super(context);
@@ -42,9 +49,8 @@ public class ProgressView extends View {
     private void initPaint() {
         paint = new Paint();
 
-        paint.setColor(paintColor);
         paint.setAntiAlias(true);
-        paint.setStrokeWidth(strokeWidth);
+
 
         /*
         * 三种样式
@@ -79,11 +85,35 @@ public class ProgressView extends View {
         * 3.画 文字
         * */
         //圆心
+        strokeWidth = UIUtils.dp2px(10);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setColor(paintColor);
         int cx = width / 2;
         int cy = heigth / 2;
 
         int radius = cx - strokeWidth / 2;
         canvas.drawCircle(cx,cy,radius,paint);
+
+        //画弧
+        paint.setColor(Color.RED);
+        RectF rectF = new RectF();
+        rectF.set(strokeWidth/2,strokeWidth/2,width-strokeWidth/2,heigth-strokeWidth/2);
+        canvas.drawArc(rectF,0,progress * 360 / max,false,paint);
+
+        //画文本
+        String text = progress * 100 / max + "%" ;
+        paint.setStrokeWidth(0);
+        paint.setTextSize(30);
+
+        Rect rect = new Rect();
+        paint.getTextBounds(text,0,text.length(),rect);
+
+        int textWidth = rect.width();
+        int textHeight = rect.height();
+        float textX = width / 2 - textWidth/2;
+        float textY = heigth / 2 + textHeight/2;
+
+        canvas.drawText(text,textX,textY,paint);
 
     }
 }
