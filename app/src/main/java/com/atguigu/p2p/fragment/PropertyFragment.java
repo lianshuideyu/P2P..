498830a7deb956ee9f48047448266d99467e0f69
lianshuideyu <834893479@qq.com>
2017-06-26 13:24:13 +0800

    资产模板的布局

--- app/src/main/java/com/atguigu/p2p/fragment/PropertyFragment.java
+++ app/src/main/java/com/atguigu/p2p/fragment/PropertyFragment.java
@@ -1,12 +1,40 @@
 package com.atguigu.p2p.fragment;
 
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.atguigu.p2p.R;
 import com.atguigu.p2p.base.BaseFragment;
+
+import butterknife.InjectView;
 
 /**
  * Created by Administrator on 2017/6/20.
  */
 
 public class PropertyFragment extends BaseFragment {
+
+    @InjectView(R.id.tv_settings)
+    TextView tvSettings;
+    @InjectView(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @InjectView(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @InjectView(R.id.tv_me_name)
+    TextView tvMeName;
+    @InjectView(R.id.rl_me)
+    RelativeLayout rlMe;
+    @InjectView(R.id.recharge)
+    ImageView recharge;
+    @InjectView(R.id.withdraw)
+    ImageView withdraw;
+    @InjectView(R.id.ll_touzi)
+    TextView llTouzi;
+    @InjectView(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @InjectView(R.id.ll_zichan)
+    TextView llZichan;
 
     @Override
     protected void initData(String json) {
@@ -15,12 +43,12 @@ public class PropertyFragment extends BaseFragment {
 
     @Override
     protected String getChildUrl() {
-        return  "";
+        return "";
     }
 
     @Override
     public int setLayoutId() {
-        return 0;
+        return R.layout.fragment_property;
     }
 
     @Override
@@ -32,4 +60,5 @@ public class PropertyFragment extends BaseFragment {
     protected void initData() {
 
     }
+
 }
